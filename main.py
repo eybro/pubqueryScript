5396778afef6846ee51e7c993ec860efa95cc634
erikrab <erikrab@kth,se>
2023-03-07 20:33:00 +0100

    intial

--- main.py
+++ main.py
@@ -4,37 +4,42 @@ import pymysql
 import datetime
 import os
 
-PAGE_LIST = ['bergsklubbmasteri','Klubbmasteriet','openklubbmasteri','kfkflygsektionen','ProgramRadet', 'BBQlubbmasteri',
-             'qbmrosagrisen','datasklubbmasteri','clubwasteriet','fysiksklubbmasteri','kemisklubbmasteri', 'festerietarkitektur','IndustriellEkonomiKTH']
-WORD_LIST = ['THIS','TODAY','TOMORROW', 'AT', 'MON,','TUE,','WED,','THU,','FRI,','SAT,','SUN,']
-DAY_DICT = {'MONDAY': 0, 'TUESDAY' : 1., 'WEDNESDAY': 2, 'THURSDAY': 3, 'FRIDAY': 4, 'SATURDAY': 5, 'SUNDAY': 6}
+# List of fb names for all pubs
+PAGE_LIST = ['bergsklubbmasteri', 'Klubbmasteriet', 'openklubbmasteri', 'kfkflygsektionen', 'ProgramRadet',
+             'BBQlubbmasteri',
+             'qbmrosagrisen', 'datasklubbmasteri', 'clubwasteriet', 'fysiksklubbmasteri', 'kemisklubbmasteri',
+             'festerietarkitektur', 'IndustriellEkonomiKTH']
+# Keywords to look for
+WORD_LIST = ['THIS', 'TODAY', 'TOMORROW', 'AT', 'MON,', 'TUE,', 'WED,', 'THU,', 'FRI,', 'SAT,', 'SUN,']
+DAY_DICT = {'MONDAY': 0, 'TUESDAY': 1., 'WEDNESDAY': 2, 'THURSDAY': 3, 'FRIDAY': 4, 'SATURDAY': 5, 'SUNDAY': 6}
+
 
 def get_events(page):
     d = {}
-    for post in get_posts(page, pages = 2,options={"allow_extra_requests": False}, cookies='cookie-file.txt'):
+    for post in get_posts(page, pages=2, options={"allow_extra_requests": False}, cookies='cookie-file.txt'):
         post_list = post['text'].split()
         try:
-            if(post_list[0] in WORD_LIST):
+            if post_list[0] in WORD_LIST:
                 split = post['text'].split("\n")
-                if(post_list[0] == 'THIS'):
+                if post_list[0] == 'THIS':
                     today = datetime.date.today()
-                    date = today + datetime.timedelta(days=DAY_DICT[post_list[1]]-today.weekday())
+                    date = today + datetime.timedelta(days=DAY_DICT[post_list[1]] - today.weekday())
                 else:
                     split = post['text'].split("\n")
                     date = dateparser.parse(split[0])
-                if(date >= date.today()):
-                    if (len(split)>1):
+                if date >= date.today():
+                    if len(split) > 1:
                         month = date.month
                         day = date.day
-                        if(int(date.month) < 10):
-                            month = '0'+ str(date.month)
-                        if(int(date.day) < 10):
-                            day = '0'+ str(date.day)
-                        d[str(date.year)+"-"+str(month)+"-"+str(day)] = [split[1],page]
+                        if int(date.month) < 10:
+                            month = '0' + str(date.month)
+                        if int(date.day) < 10:
+                            day = '0' + str(date.day)
+                        d[str(date.year) + "-" + str(month) + "-" + str(day)] = [split[1], page]
                     else:
                         d[date] = 'Pub'
 
-        except Exception as e:
+        except:
             pass
     return d
 
@@ -44,27 +49,26 @@ def insert(dict):
         title = list[0]
         page = list[1]
 
-        insert = True
+        insert_ok = True
 
-        if(page=='IndustriellEkonomiKTH'):
+        # filter out non pub/club events
+        if page == 'IndustriellEkonomiKTH':
             title_s = title.split(' ')
-            insert = False
+            insert_ok = False
             for word in title_s:
                 if 'pub' in word or 'club' in word or 'PUB' in word or 'CLUB' in word or 'Pub' in word or 'Club' in word:
-                    insert = True
-					
+                    insert_ok = True
 
-        if(insert):
+        if insert_ok:
             # print(title,page)
             with connection.cursor() as cursor:
                 sql = "INSERT IGNORE INTO events (`page`, `title`, `date`) VALUES (%s, %s, %s)"
-                cursor.execute(sql, (page,title,date))
+                cursor.execute(sql, (page, title, date))
             connection.commit()
 
 
+connection = pymysql.connect(host='mysql115.unoeuro.com', user='pubquery_se', password='DhRd9w3zxpGftergFkny',
+                             db='pubquery_se_db', cursorclass=pymysql.cursors.DictCursor)
 
-
-connection = pymysql.connect(host = 'mysql115.unoeuro.com',user = 'pubquery_se', password = 'DhRd9w3zxpGftergFkny',db = 'pubquery_se_db', cursorclass=pymysql.cursors.DictCursor)
-
-for page in PAGE_LIST:
-    insert(get_events(page))
+for page_name in PAGE_LIST:
+    insert(get_events(page_name))
